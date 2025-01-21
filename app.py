44f824eed8df6a5b1bd9ffcd3f71af34854b0e2d
arij <m'hiri.arij@esprit.tn>
2025-01-21 23:42:58 +0100

    frontend works

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pdfplumber
 import language_tool_python
 import io
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initializing FastAPI and language tool
 app = FastAPI()
@@ -25,6 +26,15 @@ def correct_text(text):
     matches = tool.check(text)
     corrected_text = language_tool_python.utils.correct(text, matches)
     return corrected_text
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 # Upload Endpoint
 @app.post("/upload/")
